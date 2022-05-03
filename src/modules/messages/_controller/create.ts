eef85b957d3f2469c79f96db09c9cfc5a6ce02a6
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-05-03 17:48:31 +0800

    added updating of user health status

--- src/modules/messages/_controller/create.ts
+++ src/modules/messages/_controller/create.ts
@@ -13,7 +13,8 @@ export const createOneController: RequestHandler = async (req, res, next) => {
 		const authToken = process.env.TWILIO_AUTH_TOKEN;
 		const client = require("twilio")(accountSid, authToken);
 
-		const systemNumber = "+19377213296";
+		// const systemNumber = "+19377213296";
+		const systemNumber = "+17372347216";
 		// get the users mobile number
 		const clientNumber = `+63${req.body.mobileNumber}`;
 		// generate 4 ramdom otp code
@@ -34,7 +35,7 @@ export const createOneController: RequestHandler = async (req, res, next) => {
 					.json({ success: true, otp: otp, message: message })
 			)
 			.catch((err) => {
-				console.log(err.response)
+				console.log(err)
 				res.status(200).json({ success: false, message: err })	
 			});
 	} catch (err) {

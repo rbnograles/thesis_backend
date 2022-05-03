cbe14f53b7293d61cc1b212faee061e621f790ab
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-05-03 17:29:51 +0800

    added updating of user health status

--- src/modules/positive-logs/_services/put.ts
+++ src/modules/positive-logs/_services/put.ts
@@ -4,7 +4,7 @@ import { UserAccountModel } from "../../users/model";
 export const putOneRecoveredService = async (
 	data: any
 ): Promise<Boolean> => {
-	await PositiveLogsModel.findOneAndUpdate({ mobileNumber: data.mobileNumber }, { userHealthStatus: "Recovered" }, { returnOriginal: false });
+	await PositiveLogsModel.findOneAndUpdate({ mobileNumber: data.mobileNumber }, { healthStatus: "Recovered" }, { returnOriginal: false });
     await UserAccountModel.findOneAndUpdate({ mobileNumber: data.mobileNumber }, { userHealthStatus: "Recovered" }, { returnOriginal: false });
     return true;
 };

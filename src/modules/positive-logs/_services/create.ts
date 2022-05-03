1ddf681fa0e66ebca0ed582891ef210bc7cc0cf9
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-05-03 16:49:47 +0800

    added updating of user health status

--- src/modules/positive-logs/_services/create.ts
+++ src/modules/positive-logs/_services/create.ts
@@ -1,8 +1,10 @@
 import { IPositiveLogsType, PositiveLogsModel } from "../model";
+import { UserAccountModel } from "../../users/model";
 
 export const createOneService = async (
 	data: any
 ): Promise<IPositiveLogsType> => {
 	const result = await PositiveLogsModel.create(data);
+	await UserAccountModel.findOneAndUpdate({ mobileNumber: data.mobileNumber }, { userHealthStatus: "Positive" }, { returnOriginal: false });
 	return result;
 };

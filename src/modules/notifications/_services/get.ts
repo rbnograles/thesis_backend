f7fa36bd4656820f014fb38934dbf70669585d03
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-05-05 14:43:57 +0800

    notification fixes

--- src/modules/notifications/_services/get.ts
+++ src/modules/notifications/_services/get.ts
@@ -1,7 +1,9 @@
-import { NotificationModel, INotification } from "../model";
+import { NotificationModel } from "../model";
+import { UserAccountModel } from "../../users/model";
 
 export const getOneService = async (id: string) => {
-	const result = await NotificationModel.find();
+	const userAccount = await UserAccountModel.findById(id)
+	const result = await NotificationModel.find({ mobileNumber: userAccount.mobileNumber });
 	return result;
 };
 

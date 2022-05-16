6a9e938bfb500c3acdef406de348c213377d3922
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-05-16 14:37:10 +0800

    revert to testing account

--- src/modules/users/model.ts
+++ src/modules/users/model.ts
@@ -9,28 +9,33 @@ export type UserSchema = {
 };
 
 const userAccountSchema = new Schema({
+	// @ts-ignore
 	mobileNumber: {
 		type: String,
 		required: true,
 	},
+	// @ts-ignore
 	userHealthStatus: {
 		type: String,
 		default: "Normal",
 	},
+	// @ts-ignore
 	userType: {
 		type: String,
 		default: "Member",
 	},
+	// @ts-ignore
 	isVerified: {
 		type: Boolean,
 		default: false,
 	},
+	// @ts-ignore
 	createdAt: {
 		type: Date,
 		default: Date.now(),
 	},
 });
-
+// @ts-ignore
 export const UserAccountModel = model<IUserAccountType>(
 	"user",
 	userAccountSchema

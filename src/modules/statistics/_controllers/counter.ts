85aafb86798778e3791ae4e68ec867c622888a39
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-05-19 12:59:03 +0800

    changes on counter

--- src/modules/statistics/_controllers/counter.ts
+++ src/modules/statistics/_controllers/counter.ts
@@ -6,14 +6,14 @@ export const appStatisticsController: RequestHandler = async (req, res, next) =>
 
     const totalUserCount = await UserAccountModel.find({});
     const totalActiveCases = await PositiveLogsModel.find({ healthStatus: "Positive" });
-    const totalRecoveredCases = await PositiveLogsModel.find({ healthStatus: "Recivered" });
-    const totalCloseContactCases = await PositiveLogsModel.find({});
+    const totalRecoveredCases = await PositiveLogsModel.find({ healthStatus: "Recovered" });
+    const totalNormalUsers = await UserAccountModel.find({ userHealthStatus: "Normal" });
 
     return res.status(200).json({ 
         success: true, 
         totalUserCount: totalUserCount.length, 
         totalActiveCases: totalActiveCases.length, 
         totalRecoveredCases: totalRecoveredCases.length, 
-        totalCloseContactCases: totalCloseContactCases.length 
+        totalNormalUsers: totalNormalUsers.length 
     });
 };

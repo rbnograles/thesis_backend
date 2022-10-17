9ea198e05a10abfda7777bdeee903f991835d5d9
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-17 18:51:01 +0800

    remove message bird from code base

--- src/_utils/messageSender.ts
+++
@@ -1,9 +0,0 @@
-import initMB from "messagebird";
-import dotenv from "dotenv";
-
-// set up to message bird configuration
-dotenv.config();
-const accessKey = process.env.messageBirdTestKey
-
-export const messagebird = initMB(accessKey)
-
